5439761dcd3b8112ee008cc7d2533271776933da
wafflebitez <wolfbitez23@gmail.com>
2024-09-10 21:49:57 -0400

    refactor/include comments
    trying to be more compliant with pylint

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,10 @@
 import logging
+import sys
 from utils.jsonfile import JsonFile
+
+"""
+This module handles the JSON configuration file.
+"""
 
 DEFAULT_CONFIG = {
     'token': 'token_here',
@@ -11,6 +16,13 @@ DEFAULT_CONFIG = {
 }
 
 class Config(JsonFile):
+
+    """
+    A class to handle configuration for the application, extending JSON file operations.
+
+    :param str filepath: The path to the configuration file.
+    """
+
     def __init__(self, filepath='config.json'):
         super().__init__(filepath)
         config_data = self.load()
@@ -18,6 +30,6 @@ class Config(JsonFile):
             logging.info('Auto-generating a new configuration file.')
             self.save(DEFAULT_CONFIG)
             logging.warning('Please modify the newly generated config and restart the application.')
-            exit()
+            sys.exit()
         self.__dict__.update(config_data)
         logging.info('Successfully loaded the configuration file.')
--- utils/jsonfile.py
+++ utils/jsonfile.py
@@ -2,25 +2,43 @@ import json
 import logging
 import os
 
+"""
+This module handles JSON file operations.
+"""
+
 class JsonFile:
+
+    """
+    A class to manage JSON file reading and writing.
+
+    :param str filepath: The path of the JSON file to read/write to
+    """
+
     def __init__(self, filepath):
         self.filepath = filepath
-        
+
     def load(self):
+        """
+        Loads the JSON file from the filepath 
+        """
         if not os.path.exists(self.filepath):
-            logging.error(f'"{self.filepath}" not found!')
+            logging.error('"%s" not found!', self.filepath)
             return None
         try:
-            with open(self.filepath, 'r') as file:
+            with open(self.filepath, 'r', encoding='utf-8') as file:
                 return json.load(file)
-        except Exception as e:
-            logging.error(f'Failed to load {self.filepath}: {e}')
-            return None
+        except json.JSONDecodeError as e:
+            logging.error('Failed to decode JSON from "%s": %s', self.filepath, e)
 
     def save(self, data):
+        """
+        Saves the JSON file
+
+        :param dict data: The data to be written in the JSON file
+        """
         try:
-            with open(self.filepath, 'w') as file:
+            with open(self.filepath, 'w', encoding='utf-8') as file:
                 json.dump(data, file, indent=4)
-            logging.info(f'"{self.filepath}" successfully saved.')
+            logging.info('"%s" successfully saved.', self.filepath)
         except Exception as e:
-            logging.error(f'Failed to save "{self.filepath}": {e}')
+            logging.error('Failed to save "%s": %s', self.filepath, e)
